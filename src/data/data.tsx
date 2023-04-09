599fb68e0cb4d7bc56a1a5fb911d3279ee918297
Pranay-Kumar-1215 <kumarpranay1215@gmail.com>
2023-04-09 15:56:49 +0530

    nv

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -53,7 +53,7 @@ export const SectionId = {
   Resume: 'resume',
   Skills: 'skills',
   Stats: 'stats',
-  Testimonials: 'testimonials',
+  Testimonials: 'Certifications',
 } as const;
 
 export type SectionId = typeof SectionId[keyof typeof SectionId];
